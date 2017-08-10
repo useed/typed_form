5f1bbb0cba23cf75e214febdcb11cf7f1bb46240
robcole <robcole@gmail.com>
2017-08-10 16:22:23 -0700

    Strip hex-formatted non-breaking UTF-8 spaces
    Typeform recently began sending an additional representation
    of breaking space (hex-formatted). This adds it to the list
    of characters in the space normalizing regex, replacing
    it with a normal space.
    
    Adds \xC2\xA0 in the single form example JSON, to ensure
    that normalization is working on both sets of possible spaces.

--- lib/typed_form/util.rb
+++ lib/typed_form/util.rb
@@ -4,7 +4,7 @@ module TypedForm
     # Removes non-breaking spaces (character point 160) from TypeForm data
     # before beginning processing.
     def self.normalize_spaces(text)
-      text.gsub("\\u00a0", " ").gsub(/[[:space:]]/, " ")
+      text.gsub(/(\\u00a0|\\xC2\\xA0)/, " ").gsub(/[[:space:]]/, " ")
     end
   end
 end
